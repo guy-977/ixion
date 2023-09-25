9bda76e6bc3408ab8e40a47174589ccf12b3bb4a
xXA2005 <A2005BALILA@gmail.com>
2023-09-25 19:11:05 +0300

    ahdb to footer + removed the team from header

--- app/components/foot.tsx
+++ app/components/foot.tsx
@@ -1,7 +1,34 @@
 export default function foot() {
-    return (<div className="relative bottom-0 bg-[#000] w-full h-16 text-center">Copy rights to team RefineAI 2023
-    &nbsp;<a href="https://guy-977.github.io/" className="from-[#33cc33] to-[#006622] bg-gradient-to-r bg-clip-text text-transparent">Ahmed AL-Refai</a>
-    &nbsp;<a href="https://ab5.wtf" className="from-[#33cc33] to-[#006622] bg-gradient-to-r bg-clip-text text-transparent">Abdullah Balila</a>
-    &nbsp;<a href="https://ab5.wtf" className="from-[#33cc33] to-[#006622] bg-gradient-to-r bg-clip-text text-transparent">Missoum Yusof</a>
-    </div>)
+  return (
+    <div className="relative bottom-0 bg-[#000] w-full h-16 text-center">
+      Copy rights to team RefineAI 2023 &nbsp;
+      <a
+        href="https://guy-977.github.io/"
+        className="from-[#33cc33] to-[#006622] bg-gradient-to-r bg-clip-text text-transparent"
+      >
+        Ahmed AL-Refai
+      </a>
+      &nbsp;
+      <a
+        href="https://ab5.wtf"
+        className="from-[#33cc33] to-[#006622] bg-gradient-to-r bg-clip-text text-transparent"
+      >
+        Abdullah Balila
+      </a>
+      &nbsp;
+      <a
+        href="https://www.linkedin.com/in/missoum-yusuf-19a445207/"
+        className="from-[#33cc33] to-[#006622] bg-gradient-to-r bg-clip-text text-transparent"
+      >
+        Missoum Yusof
+      </a>
+      &nbsp;
+      <a
+        href="https://github.com/ahdbarjoud?tab=repositories"
+        className="from-[#33cc33] to-[#006622] bg-gradient-to-r bg-clip-text text-transparent"
+      >
+        Ahmed Barjoud
+      </a>
+    </div>
+  );
 }
